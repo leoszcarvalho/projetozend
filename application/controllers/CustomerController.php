8aa7dad9393cd378d22062bfb00987a49a2f63a9
leonardo <leonardo.souzas30@gmail.com>
2015-01-16 20:56:09 -0200

    Commit de mudanças de DB

--- application/controllers/CustomerController.php
+++ application/controllers/CustomerController.php
@@ -17,6 +17,56 @@ class CustomerController extends Zend_Controller_Action
     {
         $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/forms/customer.ini', 'create');
         $this->view->form = new Application_Form_Customer($config);
+        $dbTable = new Application_Model_DbTable_Customer();
+        
+        //Seleção de registro
+        //$result = $dbTable->fetchRow();
+
+        
+        //Insert de registro
+        /*$data = array(
+            'cpf' => '66555',
+            'name' => 'griffin',
+            'gender' => 'male',
+            'phone' => '311561651',
+            'cellphone' => '984516156',
+            'email' => 'griffin@familyguy.com.br',
+            'street' => 'unknow',
+            'house_number' => '32',
+            'neighborhood' => 'unknow',
+            'cep' => '321241',
+            'city' => 'uashda',
+            'status' => 'active',
+            'monthly_payment' => '30',
+            'maturity' => '32',
+            'notes' => 'asudhasud',
+            
+        );
+        
+        
+        if($insert = $dbTable->insert($data))
+        {
+            echo "xablau";
+        }
+        else
+        {
+            echo "nah";
+            
+        }*/ 
+        
+        //Update de registro
+        /*
+        $dataUpdate = array(
+            'name' => 'fernando'
+        );
+        
+        
+        $update = $dbTable->update($dataUpdate, "name = 'carlos'");
+        */
+        
+        //Delete de registro
+        //$delete = $dbTable->delete(array('id = ?' => '13'));
+        
     }
 
     public function editAction()
